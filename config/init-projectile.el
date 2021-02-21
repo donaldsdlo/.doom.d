efbbc2bcadc428b8ea497971166940c60715352e
Donald Lo <donald.sd.lo@gmail.com>
2021-02-21 20:29:14 +0800

    projectile

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -4,6 +4,7 @@
                                                   ".pdf"
                                                   ".aux"
                                                   ".toc"
+                                                  ".out"
                                                   ))
 
 (provide 'init-projectile)
